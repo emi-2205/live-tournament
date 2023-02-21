706f92905dd20dae0dfd36a4f568aed1c1440d0f
emi-2205 <emilio.grieco98@gmail.com>
2023-02-22 00:06:41 +0100

    responsiveness

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     @foreach ($partite as $partita)
         <div class="flex" style="justify-content: center;">
-            <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+            <a href="https://laravel.com/docs" class="content-container scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                 <div class="grid-container-outer gap-6 lg:gap-8" style="justify-content: space-around; width: 100%;">
 
                     <div class="teams">
